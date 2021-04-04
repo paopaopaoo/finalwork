3ef4f7e240e262234539796f2e958081b2d7bf6c
77 <63863553+paopaopaoo@users.noreply.github.com>
2021-04-04 17:09:21 +0800

    Initial commit

---
+++ src/main/java/com/gou/finalwork/FinalworkApplication.java
@@ -0,0 +1,13 @@
+package com.gou.finalwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalworkApplication.class, args);
+	}
+
+}
